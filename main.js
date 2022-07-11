6568923d898d1bb361a395b668afa69f96591bf7
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2022-07-11 17:02:39 +0300

    some updates

---
+++ main.js
@@ -0,0 +1,45 @@
+function dropUp(){
+    document.getElementById('settings_list').classList.toggle('settings_toggle')
+}
+let myNodeList = document.getElementsByTagName('LI');
+let i;
+for (i = 0; i < myNodeList.length; i++){
+    let span = document.createElement('SPAN')
+    let txt = document.createTextNode('\u00D7')
+    span.className = "close";
+    span.appendChild(txt)
+    myNodeList(i).appendChild(span);
+}
+let close = document.getElementsByClassName('close');
+    for(i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+            let div = this.parentElement;
+            div.style.display = 'none'
+        }
+    }
+
+function newTask(){
+    let li = document.createElement('LI');
+    let inputValue = document.getElementById('new_task_input').value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue === ''){
+        alert('In order to add a task -- you must write something innit')
+    } else{
+        document.getElementById('task_items').appendChild(li)
+}
+document.getElementById('new_task_input').value = "";
+
+let span = document.createElement('SPAN');
+let txt = document.createElement('\u00D7');
+span.className = 'close';
+span.appendChild(txt);
+li.appendChild(span)
+
+
+for(i = 0; i < close.length; i++){
+    close[i].onclick = function(){
+        let div = this.parentElement;
+        div.style.display = 'none'
+    }
+}}
