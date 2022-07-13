ef5311d1f395a62a89939e7169eafe83b2fb854a
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2022-07-13 21:51:56 +0300

    settings btn fixed, design reworked

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// dropup settings button (yet unfinished)
 function dropUp(){
     document.getElementById('settings_list').classList.toggle('settings_toggle')
 }
 
+// i kinda copied it, i have not yet quite got what it does
 let myNodeList = document.getElementsByTagName('LI');
 let i;
 for (i = 0; i < myNodeList.length; i++){
@@ -20,6 +22,7 @@ let close = document.getElementsByClassName('close');
         }
     }
 
+// adding a task func, -- disappearing after being added
 function newTask(){
     let li = document.createElement('LI');
     let inputValue = document.getElementById('new_task_input').value;
@@ -38,10 +41,24 @@ span.className = "close";
 span.appendChild(txt);
 li.appendChild(span)
 
-
 for (i = 0; i < close.length; i++){
     close[i].onclick = function() {
         let div = this.parentElement;
         div.style.display = "none"
     }
 }}
+
+// checked tasks func, -- doesn't work
+let list = document.querySelector("ul");
+
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked')
+    }
+})
+// active/completed/deleted button
+
+let act_btn = document.getElementsByClassName("active_mode_button");
+act_btn.addEventListener('onclick', function(status){
+
+})
